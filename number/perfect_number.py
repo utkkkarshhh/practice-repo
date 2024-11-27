eeb45698b70934db787cc02b349be90368b50637
Utkarsh Bhardwaj <utkarshbhardwajmail@gmail.com>
2024-11-27 09:56:44 +0530

    added new files

--- number/perfect_number.py
+++ number/perfect_number.py
@@ -3,17 +3,25 @@ def perfect_number(num: int):
     """""
     A Perfect number is the one where the sum of all the divisors of a number are equal to the number it self
     """""
-    all_divisors = find_all_divisors(num)
-    if sum(all_divisors) == num:
+    sum_divisors = sum_of_divisors(num)
+    if sum_divisors == num:
         return f"{num} is a perfect number!"
     return f"{num} is not a perfect number!"
 
-def find_all_divisors(num: int) -> list:
-    divisors = []
-    for i in range(1, num):
+def sum_of_divisors(num: int) -> list:
+    if num <= 1:
+        return 0
+    
+    sum = 1
+    limit = int(num **0.5) 
+    
+    for i in range(2, limit + 1):
         if num % i == 0:
-            divisors.append(i)
-    return divisors
+            sum += i
+            if i != num // i:
+                sum += num // 1
+                
+    return sum
 
 print(perfect_number(6))
 print(perfect_number(28))
---
+++ number/sum_of_digits.py
@@ -0,0 +1,3 @@
+def sum_of_digits():
+    pass
+
