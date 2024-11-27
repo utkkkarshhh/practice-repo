138f744b956bb8d7c517b48d37629ebaa5877110
Utkarsh Bhardwaj <utkarshbhardwajmail@gmail.com>
2024-11-27 10:18:53 +0530

    added power programs

---
+++ number/power_of_number.py
@@ -0,0 +1,7 @@
+def power_of_number(number: int, power: int) -> int:
+    """
+    Calculate the power of a given number.
+    """
+    return number ** power
+
+print(power_of_number(number = 10, power = 3))
