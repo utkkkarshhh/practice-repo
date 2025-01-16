e852e32c82fe924684df13217c00da9d6e84a893
Utkarsh Bhardwaj <utkarshbhardwajmail@gmail.com>
2025-01-16 21:51:24 +0530

    update: added new linked list code

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -28,26 +28,70 @@ class LinkedList:
             new_node.next = self.head
             self.head = new_node
     
-    def delete(self, data): #Delete with the data 
-        if self.is_empty():
+    def delete(self, data):
+        if not self.head:
             print("Linked List is empty!")
             return
         if self.head.data == data:
             self.head = self.head.next
             return
+
         current = self.head
-        while current.next and current.next.data!= data:
+        while current.next and current.next.data != data:
             current = current.next
+
+        if not current.next:
+            print(f"Data {data} not found in the linked list.")
+            return
+
+        current.next = current.next.next
         
         
-    def delete_starting(self): #Delete the starting element
-        pass
+    def delete_starting(self):
+        if not self.head:
+            print("The list is empty, nothing to delete!")
+            return
+
+        self.head = self.head.next
     
     def delete_ending(self): #Delete the ending element
-        pass
+        if not self.head:
+            print("The list is empty, nothing to delete")
+            return
+
+        if not self.head.next:
+            self.head = None
+            return
+
+        current = self.head
+        while current.next and current.next.next:
+            current = current.next
+
+        current.next = None
+
     
     def search(self, data): #Search the given element in the linked list
         pass
     
     def display(self): #Display the entire linked list
-        pass
+        current = self.head
+        if not current:
+            print("The linked list is empty.")
+            return
+        while current:
+            print(current.data, end=" -> " if current.next else " -> NULL \n")
+            current = current.next
+
+ll = LinkedList()
+
+ll.append(10)
+ll.append(20)
+ll.append(30)
+ll.append(40)
+ll.append(50)
+ll.append(60)
+
+ll.display()
+ll.delete_starting()
+ll.delete_ending()
+ll.display()
