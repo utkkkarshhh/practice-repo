336216c05d01fd1274bce1a3cb53825c2329e239
Utkarsh Bhardwaj <utkarshbhardwajmail@gmail.com>
2025-02-21 01:15:59 +0530

    fix: updated the stack using linked list

--- stacks_and_queues/stacks/stack_using_linked_list.py
+++ stacks_and_queues/stacks/stack_using_linked_list.py
@@ -5,29 +5,32 @@ class Node:
 
 class Stack:
     def __init__(self):
-        self.top = None 
-        
+        self.top = None
+
     def is_empty(self):
         return self.top is None
-    
+
     def push(self, item):
         new_node = Node(item)
-        new_node.next = self.top
-        self.top = new_node
-        print(f"Pushed {item} onto the stack!")
-        
+        if self.is_empty():
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        print(f"Pushed item {item} onto the stack!")
+
     def pop(self):
         if self.is_empty():
-            print(f"Stack is empty nothing to pop!")
+            return f"Stack is already empty! Nothing to pop!"
         else:
-            popped_item = self.top.data
+            popped_item = self.top
             self.top = self.top.next
-            print(f"Popped {popped_item} from the stack!")
-            
+            print(f"Popped item {popped_item.data} from the stack")
+
     def peek(self):
         return self.top.data if self.top else "Stack is empty!"
-            
-            
+
+
 my_stack = Stack()
 
 print("Is the stack empty?", my_stack.is_empty())
